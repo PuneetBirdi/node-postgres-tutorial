b293d59dc66065da7b7655b79bc0f240be2d34f2
Puneet <birdipuneet@gmail.com>
2021-02-22 17:25:21 -0500

    Added handlebars and main view layouts for the UI.

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ db.authenticate().then(()=>{
 
 const app = express();
 
+//Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => res.send('INDEX'))
 
 //gig routes
--- routes/gigs.js
+++ routes/gigs.js
@@ -3,14 +3,39 @@ const router = express.Router();
 const db = require('../config/database');
 const Gig = require('../models/Gig');
 
+
+//Get all gigs
 router.get('/', (req, res) => 
 Gig.findAll()
     .then(gigs => {
-        console.log(gigs)
-        res.sendStatus(200);
+        res.render('gigs', {gigs})
     })
     .catch(err => console.log(err))
 )
+
+//Add a gig
+router.get('/add', (req, res) =>{
+    const data = {
+        title: 'Looking for a Wordpress Developer.',
+        technologies: 'wordpress, php, html, css',
+        budget: '$1000',
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro tempore sapiente corrupti laborum culpa, quaerat inventore dolores iste facilis, veniam cumque sunt quam, consequatur explicabo.",
+        contact_email: 'user2@gmail.com'
+    }
+
+    let { title, technologies, budget, description, contact_email } = data;
+
+    //Insert into the table
+    Gig.create({
+        title,
+        technologies,
+        budget,
+        description,
+        contact_email
+    }).then(gig =>{
+        res.redirect('/gigs')
+    }).catch(error => console.log(error))
+})
 
 
 module.exports = router;
