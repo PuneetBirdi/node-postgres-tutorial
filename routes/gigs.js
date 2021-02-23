b44a3493154bb446c431d0a7e3219d0eef779787
Puneet <birdipuneet@gmail.com>
2021-02-23 14:27:30 -0500

    Added form validation for adding new gigs.

--- routes/gigs.js
+++ routes/gigs.js
@@ -19,17 +19,45 @@ router.get('/add', (req, res) => res.render('add'))
 //Add a gig
 router.post('/add', (req, res) =>{
     let { title, technologies, budget, description, contact_email } = req.body;
+    const errors = [];
 
-    //Insert into the table
-    Gig.create({
-        title,
-        technologies,
-        budget,
-        description,
-        contact_email
-    }).then(gig =>{
-        res.redirect('/gigs')
-    }).catch(error => console.log(error))
+    //Validate fields
+    if(!title){
+        errors.push({text: 'Please add a title.'})
+    }
+    if(!technologies){
+        errors.push({text: 'Please add technologies.'})
+    }
+    if(!description){
+        errors.push({text: 'Please add a description.'})
+    }
+    if(!contact_email){
+        errors.push({text: 'Please add a contact email.'})
+    }
+
+    //Check for errors
+    if(errors.length > 0){
+        res.render('add', {
+            errors,
+            title,
+            technologies,
+            budget,
+            description,
+            contact_email
+        })
+    }else{
+        //Insert into the table
+        Gig.create({
+            title,
+            technologies,
+            budget,
+            description,
+            contact_email
+        }).then(gig =>{
+            res.redirect('/gigs')
+        }).catch(error => console.log(error))
+    }
+
 })
 
 
